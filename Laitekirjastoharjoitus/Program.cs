6c956d50b2d869ddb15f90d5f0da87544e590b81
AnttiJelekainen <anttijelekainen789@gmail.com>
2024-02-15 14:35:24 +0200

    Moved some fields and properties to Device class.

--- Laitekirjastoharjoitus/Program.cs
+++ Laitekirjastoharjoitus/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Laiterekisteri
 {
-    // Base class for devices
+    /* Base class for devices */
     class Device
     {
         // Fields
@@ -18,6 +18,18 @@ namespace Laiterekisteri
 
         // Properties
         // ----------
+
+        string processorType;
+        public string ProcessorType { get { return processorType; } set { processorType = value; } }
+
+
+        int amountRAM;
+        public int AmountRAM { get { return amountRAM; } set { amountRAM = value; } }
+
+
+        int storageCapacity;
+        public int StorageCapacity { get { return storageCapacity; } set { storageCapacity = value; } }
+
         public string Identity
         {
             get { return identity; }
@@ -69,26 +81,24 @@ namespace Laiterekisteri
         // -------------
     }
 
-    // Class for Computers. Inherits properties and methods from Device class.
+    /* Class for Computers | Inherits properties and methods from Device class. */
     class Computer : Device
     {
-        // Fields:
-        string processorType;
-        public string ProcessorType { get { return processorType; } set {  processorType = value; } }
-
-
-        int amountRAM;
-        public int AmountRAM { get { return amountRAM; } set { amountRAM = value; } }
-
-
-        int storageCapacity;
-        public int StorageCapacity { get { return storageCapacity; } set {  storageCapacity = value; } }
-
-
         // Constructors:
         public Computer() : base() 
             { }
 
+        public Computer(string identity) : base(identity)
+        { }
+
+        // Other methods:
+        public void ShowInfo() 
+        {
+            Console.WriteLine("Koneen nimi: " + Identity);
+            Console.WriteLine("Prosessori: " + ProcessorType);
+            Console.WriteLine("Keskusmuisti:  " +  AmountRAM);
+            Console.WriteLine("Levytila: " + StorageCapacity);
+        }
     }
 
     class SmartPhone : Device
@@ -96,7 +106,13 @@ namespace Laiterekisteri
 
     class Tablet : Device
     {
+
         // Fields
+        string operatingSystem;
+        public string OperationSystem { get { return operatingSystem; } set { operatingSystem = value; } }
+
+        bool stylusEnablet = false;
+        public bool StylusEnablet { get {  return stylusEnablet; } set { stylusEnablet = value; } }
 
         // Properties
 
@@ -108,20 +124,20 @@ namespace Laiterekisteri
     {
         static void Main(string[] args)
         {
-            // Let's create an test object from the Device class with default constructor (0 parameters)
+            // Let's create an test object from the Device class with default constructor (0 parameters):
             Device device1 = new Device();
             Console.WriteLine(device1.Identity);
 
-            // Let's create another device with identity parameter
+            // Let's create another device with identity parameter:
             Device device2 = new Device("Toinen laite");
             Console.WriteLine(device2.Identity);
 
-            // Set device2 warranty to 60 months
+            // Setting device2 warranty to 60 months:
             device2.Warranty = 60;
 
             Console.WriteLine("Takuutiedoksi päivitetty: " + device2.Warranty);
 
-            // Let's create one more device with all parameters
+            // Let's create one more device with all parameters:
             Device device3 = new Device("Kolmas kone", "8.2.2024", 150.00d, 36);
 
             Console.WriteLine(device3.Identity);
@@ -129,11 +145,23 @@ namespace Laiterekisteri
             Console.WriteLine(device3.DateBought);
             Console.WriteLine(device3.Warranty);
 
-            // Creating new Computer. Inherits properties and methods from Device class.
+            // Creating new Computer named >computer1>. Inherits properties and methods from Device class.
             Computer computer1 = new Computer();
-            Console.WriteLine("New computer name is: " + computer1.Identity);
 
-            // keeps the window open untill Enter is pressed.
+            // setting value to Processor:
+            computer1.ProcessorType = "Intel I7";
+            // setting value to RAM:
+            computer1.AmountRAM = 16;
+
+            Console.WriteLine("New computer name is: " + computer1.Identity + " and there is " + computer1.ProcessorType + " with " + computer1.AmountRAM + "GB RAM " );
+
+            // Creating new named computer with another contructor.
+            Computer computer2 = new Computer("Antin uusi supertietokone");
+            computer2.ProcessorType = "Inter Core i7 vPro";
+            computer2.AmountRAM = 32;
+            computer2.ShowInfo();
+
+            // keeps the window open untill >Enter> is pressed.
             Console.ReadLine();
         }
     }
