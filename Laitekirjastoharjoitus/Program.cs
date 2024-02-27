eccc8e18f58939805a9cf8d7efd442ce431f3bf0
AnttiJelekainen <anttijelekainen789@gmail.com>
2024-02-27 12:03:23 +0200

    While loopin käyttöönotto ohjelman pitämiseksi auki.

--- Laitekirjastoharjoitus/Program.cs
+++ Laitekirjastoharjoitus/Program.cs
@@ -152,6 +152,7 @@ namespace Laiterekisteri
 
         public void TabletInfo()
         {
+
             Console.WriteLine("Käyttöjärjestelmä: " + OperationSystem);
             Console.WriteLine("Kynätuki: " + stylusEnablet);
         }
@@ -162,74 +163,27 @@ namespace Laiterekisteri
     {
         static void Main(string[] args)
         {
-            // Let's create an test object from the Device class with default constructor (0 parameters):
-            //Device device1 = new Device();
-            //Console.WriteLine(device1.Identity);
-
-            // Let's create another device with identity parameter:
-            //Device device2 = new Device("Toinen laite");
-            //Console.WriteLine(device2.Identity);
-
-            // Setting device2 warranty to 60 months:
-            //device2.Warranty = 60;
-
-            //Console.WriteLine("Takuutiedoksi päivitetty: " + device2.Warranty);
-
-            // Let's create one more device with all parameters:
-            //Device device3 = new Device("Kolmas kone", "8.2.2024", 150.00d, 36);
-
-            //Console.WriteLine(device3.Identity);
-            //Console.WriteLine(device3.Price);
-            //Console.WriteLine(device3.DateBought);
-            //Console.WriteLine(device3.Warranty);
-
-
-            // CREATING OBJECTS AND TESTING METHODS:
-            // -------------------------------------
-
-            Console.WriteLine("--------------------------------");
-            Console.WriteLine("Tietokone 1:n hankintatiedot: ");
-            Console.WriteLine("--------------------------------");
-            // Creating new Computer named >computer1>. Inherits properties and methods from Device class.
-            Computer computer1 = new Computer();
-            // setting value to Processor:
-            computer1.ProcessorType = "Intel I7";
-            // setting value to RAM:
-            computer1.AmountRAM = 16;
-            computer1.DateBought = "15.2.2024";
-            computer1.Price = 250.00d;
-            computer1.Warranty = 12;
+            // While loop that keeps up the main program running:
+            while (true)
             {
-                computer1.ShowPurchaseInfo();
+                Console.WriteLine("Minkä laitteen tiedot tallennetaan? ");
+                Console.Write("1.) Computer, 2.) Tablet, 3.) Phone");
+                string type = Console.ReadLine();
+
+
+                // While loop exit:
+                Console.WriteLine("Haluatko jatkaa K/e");
+                string continueAnswer = Console.ReadLine();
+                continueAnswer = continueAnswer.Trim();
+                continueAnswer = continueAnswer.ToLower();
+
+                if (continueAnswer == "e")
+                {
+                    break;
+                }
+
+                
             }
-
-
-            //Console.WriteLine("New computer name is: " + computer1.Identity + " and there is " + computer1.ProcessorType + " with " + computer1.AmountRAM + "GB RAM " );
-            Console.WriteLine("");
-            Console.WriteLine("--------------------------------");
-            Console.WriteLine("Tietokone 2:n hankintatiedot: ");
-            Console.WriteLine("--------------------------------");
-            Console.WriteLine("");
-
-            // Creating new named computer with another contructor.
-            Computer computer2 = new Computer("Antin uusi supertietokone");
-            computer2.ProcessorType = "Inter Core i7 vPro";
-            computer2.AmountRAM = 32;
-            computer2.ShowPurchaseInfo();
-            computer2.ShowBasicTechInfo();
-
-            // Testing tablet
-            Console.WriteLine("");
-            Console.WriteLine("-----------------------------");
-            Console.WriteLine("Tablet 1:n hankintatiedot: ");
-            Console.WriteLine("-----------------------------");
-            Console.WriteLine("");
-            Tablet tabletti1 = new Tablet("Antin mielikuvitustabletti");
-            tabletti1.OperationSystem = "IOS";
-            tabletti1.DateBought = "6.9.1696";
-            tabletti1.StylusEnablet = true;
-            tabletti1.ShowPurchaseInfo();
-            tabletti1.TabletInfo();
 
             //// keeps the window open untill >Enter> is pressed.
             Console.ReadLine();
